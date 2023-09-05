1b85b4c4dd865f01c40bc883af7bd2e61955975a
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-09-05 18:13:16 +0100

    Burger menu on every page

--- js/header.js
+++ js/header.js
@@ -20,6 +20,35 @@ class Header extends HTMLElement {
           list-style: none;
           text-align: left;
         }
+
+        .menu .icon {
+          display: none;
+        }
+        
+        @media screen and (max-width: 600px) {
+          .menu a {
+            display: none;
+          }
+          .menu a.icon {
+            float: right;
+            display: block;
+          }
+        }
+        
+        @media screen and (max-width: 600px) {
+          .menu.responsive {position: relative;}
+          .menu.responsive a.icon {
+            position: absolute;
+            right: 0;
+            top: 0;
+          }
+        
+          .menu.responsive a {
+            float: none;
+            display: block;
+            text-align: left;
+          }
+        }
         
         li.menu-item {
           display: inline-block;
@@ -54,13 +83,26 @@ class Header extends HTMLElement {
           <img src="../images/caz-logo-2.png" alt="Caz's logo">
           <h2>CAZ DAY GESTALT THERAPY</h2>
         </a>
-        <ul class="menu">
+        <ul class="menu" id="myTopnav">
           <li class="menu-item"><a href="../index.html">Home</a></li>
           <li class="menu-item"><a href="gallery.html">Gallery</a></li>
           <li class="menu-item"><a href="testimonials.html">Testimonials</a></li>
           <li class="menu-item"><a href="articles.html">Articles, Books and Blogs</a></li>
           <li class="menu-item"><a href="appointments.html">Appointments, Fees, Location etc.</a></li>
+          <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+          <i class="fa fa-bars"></i>
+        </a>
         </ul>
+        <script>
+        function myFunction() {
+          var x = document.getElementById("myTopnav");
+          if (x.className === "menu") {
+            x.className += " responsive";
+          } else {
+            x.className = "menu";
+          }
+        }
+        </script>
       </header>
     `;
   }
